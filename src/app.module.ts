0434389a1f2c65e130d01be58719e4ec0b4f391f
aleidy-dot <aleidymillan@gmail.com>
2021-01-22 16:35:14 -0500

    multitenancy cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { ConnectionOptions } from 'typeorm';
+import { TenancyModule } from './tenancy/tenancy.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TenancyModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      async useFactory(config: ConfigService) {
+        return {
+          type: 'mysql',
+          host: config.get('DB_HOST'),
+          username: config.get('DB_USER'),
+          password: config.get('DB_PASSWORD'),
+          port: +config.get('DB_PORT'),
+          database: config.get('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+          ssl: true,
+        } as ConnectionOptions;
+      },
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number;
+
+  constructor(private readonly configService: ConfigService) {
+    AppModule.port = this.configService.get('APP_PORT');
+  }
+}
---
+++ src/tenancy/dto/read-tenant.dto.ts
@@ -0,0 +1,14 @@
+
+import { Exclude, Expose } from 'class-transformer';
+import { IsNumber, IsString } from 'class-validator';
+
+@Exclude()
+export class ReadTenantDto {
+  @IsNumber()
+  @Expose()
+  readonly id: number;
+
+  @IsString()
+  @Expose()
+  readonly name: string;
+}
